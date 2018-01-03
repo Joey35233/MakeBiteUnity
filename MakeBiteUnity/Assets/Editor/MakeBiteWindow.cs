c9ab31c2c11c5ec902cb88637e47c61a48f4c829
Joey35233 <joey35233@gmail.com>
2018-01-03 09:29:18 -0500

    Initial functionality commit

---
+++ MakeBiteUnity/Assets/Editor/MakeBiteWindow.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MakeBiteWindow : EditorWindow
+{
+    struct MakeBiteMod
+    {
+        public string modName { private get; set; }
+        public string modAuthor { private get; set; }
+        public string modVersion { private get; set; }
+        public string modGameVersion { private get; set; }
+        public string modAuthor { private get; set; }
+        string modDescription;
+    }
+
+    [MenuItem("MakeBite/Pack")]
+    public static void ShowWindow()
+    {
+        GetWindow<MakeBiteWindow>("MakeBite");
+    }
+
+    void OnGUI()
+    {
+        MakeBiteMod makeBiteMod = new MakeBiteMod();
+
+        GUILayout.Label("Mod information", EditorStyles.boldLabel);
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.BeginHorizontal();
+        makeBiteMod.modName = EditorGUILayout.DelayedTextField("Mod name", makeBiteMod.modName);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        makeBiteMod.modAuthor = EditorGUILayout.TextField("Mod author", makeBiteMod.modAuthor);
+        EditorGUILayout.EndHorizontal();
+    }
+}
